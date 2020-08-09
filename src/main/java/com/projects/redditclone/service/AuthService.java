4bd34a0d85e852ad5f6dfb1044a9727e6a5ed503
Snehangshu Chakraborty <chakraborty.snehangshu@gmail.com>
2020-08-10 02:03:49 +0530

    Created API for Sign UP and Authorisation

---
+++ src/main/java/com/projects/redditclone/service/AuthService.java
@@ -0,0 +1,47 @@
+package com.projects.redditclone.service;
+import com.projects.redditclone.dto.RegisterRequest;
+import com.projects.redditclone.model.NotificationEmail;
+import com.projects.redditclone.model.User;
+import com.projects.redditclone.model.VerificationToken;
+import com.projects.redditclone.repository.UserRepository;
+import com.projects.redditclone.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+        userRepository.save(user);
+        String token = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please Activate your account"
+                ,user.getEmail(),"http://localhost:8080/api/auth/accountVerification"+token));
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
---
+++ src/main/java/com/projects/redditclone/service/MailService.java
@@ -0,0 +1,37 @@
+package com.projects.redditclone.service;
+
+import com.projects.redditclone.exception.SpringRedditException;
+import com.projects.redditclone.model.NotificationEmail;
+import javassist.bytecode.stackmap.BasicBlock;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final MailContentBuilder mailContentBuilder;
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(NotificationEmail notificationEmail){
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("chakraborty.snehangshu@gmail.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+        try{
+            javaMailSender.send(messagePreparator);
+            log.info("Activation Email Sent to the user");
+        } catch (MailException e){
+            throw new SpringRedditException("Exception occured while sending email to "+notificationEmail.getRecipient());
+        }
+    }
+}
